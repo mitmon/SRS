7c9cad50828f6f0a1529791cfa1d759ce7294234
mitmon <mitchkrafczek@gmail.com>
2022-03-01 16:38:17 -0500

    mar012022
    - continued working on soil index
    - added a "default data" R file with default information for SRS

---
+++ R/default_data.R
@@ -0,0 +1,115 @@
+# Default data
+
+# This R file contains the default data for data tables along with default inputs.
+
+# Creation date: Mar 01, 2022
+# Last updated: Mar 01, 2022
+
+
+#' Surface available water holding capacity data table
+#'
+#' The available water holding capacity (texture) aids in the determination of
+#' the soils ability to retain and supply water to plants.
+#' @return The available water holding capacity data table.
+#' @export
+surfaceAWHCDF <- function(){
+  # First column is the P-PE for the AWHCDF
+  return(cbind(c(0,0,-50,-100,-150,-200,-250,-300,-350,-400,-450,-500),
+        c(0,40,50,60,70,80,90,95,95,95,95,95),
+        c(10,15,25,35,45,55,65,75,85,90,95,95),
+        c(20,0,0,10,20,35,50,60,70,80,90,95),
+        c(40,0,0,0,10,20,35,50,60,70,80,90),
+        c(60,0,0,0,0,10,25,40,50,60,70,80),
+        c(70,0,0,0,0,10,20,30,45,55,65,75),
+        c(75,0,0,0,0,10,20,30,45,55,65,75),
+        c(80,0,0,0,0,10,20,30,40,50,60,70),
+        c(85,0,0,0,0,10,20,30,40,50,60,70),
+        c(95,0,0,0,0,10,20,30,40,50,60,70)))
+}
+
+#' Subsurface available water holding capacity data table
+#'
+#' The available water holding capacity (texture) aids in the determination of
+#' the soils ability to retain and supply water to plants.
+#' @return The available water holding capacity data table.
+#' @export
+subsurfaceAWHCDF <- function(){
+  # First column is the average subsurface texture % clay + silt
+  return(cbind(c(0,10,20,40,60,70),
+               c(10,0,-5,-15,-25,-30),
+               c(20,5,0,-5,-10,-20),
+               c(40,15,5,0,-5,-15),
+               c(60,25,15,5,0,-5),
+               c(70,30,20,10,5,0)))
+}
+
+#' Depth of water table adjustments data table
+#'
+#' The depth of the water table effects the moisture supplying ability within
+#' the root zone.
+#' @return The water table adjustments
+#' @export
+waterTableAdjustmentDF <- function(){
+  # First column is the depth of the water table (cm)
+  # First row is the texture/drainage class
+  return(cbind(c(0,0,25,50,75,100,125),
+               c(20,100,90,60,20,0,0),
+               c(70,100,90,70,40,0,0),
+               c(100,100,90,75,50,20,0)))
+}
+
+#' Organic matter content data table
+#'
+#' The organic carbon percentage data table.
+#' @return Organic matter content data table.
+#' @export
+OMDTDF <- function(){
+  # First column is the organic carbon %
+  return(cbind(c(4,3,2,1,0.5),
+               c(0,2,5,10,15)))
+}
+
+#' Depth of topsoil data table
+#'
+#' The depth of topsoil data table.
+#' @return Depth of topsoil data table (cm)
+#' @export
+depthOfTopSoilDF <- function(){
+  # First column is the depth of the topsoil (cm)
+  return(cbind(c(20,15,10,5,0),
+               c(0,5,10,15,20)))
+}
+
+#' Surface reaction (pH) data table
+#'
+#' The surface reaction (pH) data table
+#' @return Surface reaction (pH) data table
+#' @export
+surfaceReactionDF <- function(){
+  # First column is the soil pH
+  return(cbind(c(9.0,8.5,6,5.5,5.0,4.5,4.0,3.5),
+               c(60,20,5,0,5,15,30,55,80)))
+}
+
+#' Surface salinity data table
+#'
+#' The surface salinity (dS/m) data table
+#' @return Surface salinity (dS/m) data table
+#' @export
+surfaceSalinityDF <- function(){
+  # First column is the soil salinity
+  return(cbind(c(2,4,8,16),
+               c(0,20,50,90)))
+}
+
+#' Surface sodicity data table
+#'
+#' The surface sodicity (sodium adsorption ratio) data table
+#' @return surface sodicity (sodium adsorption ratio) data table
+#' @export
+surfaceSodicityDF <- function(){
+  # First column is the soil sodicity
+  return(cbind(c(4,8,12,16,20),
+               c(60,80,120,160,160),
+               c(0,10,30,50,80)))
+}
--- R/soil_index.R
+++ R/soil_index.R
@@ -3,7 +3,7 @@
 # This library contains the soil index parameters.
 
 # Creation date: Feb 23, 2022
-# Last updated: Feb 28, 2022
+# Last updated: Mar 01, 2022
 
 #' Soil Moisture deduction
 #'
@@ -13,13 +13,343 @@
 #' texture deduction using the AWHC, surface and subsurface textures. The second
 #' part takes a removes a percentage of the deduction based on the water table
 #' depth.
-#' @param ratingTableArray Rating table lower and upper bounds for deduction.
 #' @param ppe Precipitation minus potential evapotranspiration
-#' @param temperatureFactor Input effective growing degree days or crop heat units for
-#' the study site.
-#' @return Deduction points for the basic climate rating.
+#' @param surfaceSiltPercent Percentage of surface (depths 0-60cm) silt
+#' @param surfaceClayPercent Percentage of surface (depths 0-60cm) clay
+#' @param subsurfaceSiltPercent Percentage of subsurface (depths 61-200cm) silt
+#' @param subsurfaceClayPercent Percentage of subsurface (depths 61-200cm) clay
+#' @param waterTableDepth Water table depth (in cm)
+#' @return Deduction points for the moisture deduction.
 #' @export
-# soilMoistureDeduction <- function(ratingTableArray,ppe,siltPercent,clayPercent){
-#   texture <- soilTexture(siltPercent,clayPercent)
-#
-# }
+soilMoistureDeduction <- function(ppe,surfaceSiltPercent,surfaceClayPercent,subsurfaceSiltPercent,subsurfaceClayPercent,waterTableDepth){
+
+  # 1. Available water holding capacity
+  # 1a. Surface available water holding capacity
+  if(is.na(surfaceSiltPercent) || is.na(surfaceClayPercent)){
+    surfaceAWHCDFPointDeduct <- 0
+  } else {
+    texture <- soilTexture(surfaceSiltPercent,surfaceClayPercent)
+    AWHCDF <- surfaceAWHCDF()
+    bounds <- AWHCDF[1,]
+    for(i in 1:length(bounds)){
+      if(texture < bounds[3]){
+        tempcol <- 2
+      } else if(texture < bounds[4]){
+        tempcol <- 3
+      } else if(texture < bounds[5]){
+        tempcol <- 4
+      } else if(texture < bounds[6]){
+        tempcol <- 5
+      } else if(texture < bounds[7]){
+        tempcol <- 6
+      } else if(texture < bounds[8]){
+        tempcol <- 7
+      } else if(texture < bounds[9]){
+        tempcol <- 8
+      } else if(texture < bounds[10]){
+        tempcol <- 9
+      } else if(texture < bounds[11]){
+        tempcol <- 10
+      } else {
+        tempcol <- 11
+      }
+    }
+    tempcol <- AWHCDF[,tempcol]
+    bounds <- AWHCDF[,1]
+    for(i in 1:length(AWHCDF)){
+      if(ppe > bounds[2]){
+        surfaceAWHCDFPointDeduct <- tempcol[1]
+      } else if(ppe > bounds[3]){
+        surfaceAWHCDFPointDeduct <- tempcol[2]
+      } else if(ppe > bounds[4]){
+        surfaceAWHCDFPointDeduct <- tempcol[3]
+      } else if(ppe > bounds[5]){
+        surfaceAWHCDFPointDeduct <- tempcol[4]
+      } else if(ppe > bounds[6]){
+        surfaceAWHCDFPointDeduct <- tempcol[5]
+      } else if(ppe > bounds[7]){
+        surfaceAWHCDFPointDeduct <- tempcol[6]
+      } else if(ppe > bounds[8]){
+        surfaceAWHCDFPointDeduct <- tempcol[7]
+      } else if(ppe > bounds[9]){
+        surfaceAWHCDFPointDeduct <- tempcol[8]
+      } else if(ppe > bounds[10]){
+        surfaceAWHCDFPointDeduct <- tempcol[9]
+      } else if(ppe > bounds[11]){
+        surfaceAWHCDFPointDeduct <- tempcol[10]
+      } else if(ppe > bounds[12]){
+        surfaceAWHCDFPointDeduct <- tempcol[11]
+      } else {
+        surfaceAWHCDFPointDeduct <- tempcol[12]
+      }
+    }
+  }
+
+  # 1b. Subsurface available water holding capacity
+  if(is.na(subsurfaceSiltPercent) || is.na(subsurfaceClayPercent)){
+    subsurfaceAWHCDFPointDeduct <- 0
+  } else {
+    textureminus <- surfaceSiltPercent - surfaceClayPercent
+    textureSub <- soilTexture(subsurfaceSiltPercent,subsurfaceClayPercent)
+    AWHCDF <- subsurfaceAWHCDF()
+    bounds <- AWHCDF[1,]
+    for(i in 1:length(bounds)){
+      if(textureminus < bounds[3]){
+        tempcol <- 2
+      } else if(textureminus < bounds[4]){
+        tempcol <- 3
+      } else if(textureminus < bounds[5]){
+        tempcol <- 4
+      } else if(textureminus < bounds[6]){
+        tempcol <- 5
+      } else {
+        tempcol <- 6
+      }
+    }
+    tempcol <- AWHCDF[,tempcol]
+    bounds <- AWHCDF[,1]
+    for(i in 1:length(AWHCDF)){
+      if(textureSub < bounds[3]){
+        subsurfaceAWHCDFPointDeduct <- tempcol[2]
+      } else if(textureSub < bounds[4]){
+        subsurfaceAWHCDFPointDeduct <- tempcol[3]
+      } else if(textureSub < bounds[5]){
+        subsurfaceAWHCDFPointDeduct <- tempcol[4]
+      } else if(textureSub < bounds[6]){
+        subsurfaceAWHCDFPointDeduct <- tempcol[5]
+      } else {
+        subsurfaceAWHCDFPointDeduct <- tempcol[6]
+      }
+    }
+  }
+
+  # 2. Water table adjustments
+  if(is.na(waterTableDepth)){
+    WTAPointDeduct <- 0
+  } else {
+    WTA <- waterTableAdjustmentDF()
+    bounds <- WTA[1,]
+    for(i in 1:length(bounds)){
+      if(texture < bounds[3]){
+        tempcol <- 2
+      } else if(texture < bounds[4]){
+        tempcol <- 3
+      } else {
+        tempcol <- 4
+      }
+    }
+    tempcol <- WTA[,tempcol]
+    bounds <- WTA[,1]
+    for(i in 1:length(bounds)){
+      if(waterTableDepth < bounds[3]){
+        WTAPointDeduct <- tempcol[2]
+      } else if(waterTableDepth < bounds[4]){
+        WTAPointDeduct <- tempcol[3]
+      } else if(waterTableDepth < bounds[5]){
+        WTAPointDeduct <- tempcol[4]
+      } else if(waterTableDepth < bounds[6]){
+        WTAPointDeduct <- tempcol[5]
+      } else if(waterTableDepth < bounds[7]){
+        WTAPointDeduct <- tempcol[6]
+      } else {
+        WTAPointDeduct <- tempcol[7]
+      }
+    }
+  }
+
+  # 3. Return the deduction points for the moisture deduction
+  return(sum(surfaceAWHCDFPointDeduct,subsurfaceAWHCDFPointDeduct) + (sum(surfaceAWHCDFPointDeduct,subsurfaceAWHCDFPointDeduct) * (WTAPointDeduct) / 100))
+
+}
+
+#' Interim soil rating (surface factors)
+#'
+#' The interim soil rating determines a point deduction based on the surface
+#' parameters of the soil. There are seven parameters for surface factors. These
+#' are structure and consistence (D), organic matter context (colour) (F),
+#' depth of top soil (E), reaction (pH) (V), salinity (dS/m) (N), sodicity (SAR) (Y),
+#' and peat depth (cm) (O). Currently, E, N, Y, and O are not used. Future updates
+#' will include this additional information.
+#' @param surfaceSiltPercent Percentage of surface (depths 0-60cm) silt
+#' @param surfaceClayPercent Percentage of surface (depths 0-60cm) clay
+#' @param surfaceOC Percentage of surface (depths 0-60cm) organic carbon
+#' @param depthOfTopSoil Depth of top soil up to a max of 20cm
+#' @param surfacepH Surface pH measured in saturated paste (depths 0-60cm)
+#' @param surfaceSalinity Surface salinity (depths 0-60cm)
+#' @param surfaceSodicity Surface sodicity (depths 0-60cm)
+#' @param depthOfPeat Depth of organic (peaty) surface
+#' @return Deduction points for interim soil rating.
+#' @export
+interimSoilRating <- function(surfaceSiltPercent,surfaceClayPercent,surfaceOC,depthOfTopSoil,surfacepH,surfaceSalinity,surfaceSodicity,depthOfPeat){
+
+  # 1. Structure and consistence (D)
+  # This will change in future updates
+  if(is.na(surfaceSiltPercent) || is.na(surfaceClayPercent) || is.na(surfaceOC)){
+    DPointDeduct <- 0
+  } else {
+    surfaceSandPercent <- 100 - surfaceSiltPercent - surfaceClayPercent
+    DPointDeduct <- surfaceOC
+    DPointDeduct[surfaceOC>3] <- 0
+    DPointDeduct[surfaceOC<=3] <- (3 / surfaceOC[surfaceOC<3]) + ((surfaceSandPercent[surfaceOC<3]) / 3 * surfaceOC[surfaceOC<3]) + surfaceSiltPercent[surfaceOC<3]
+
+    #Prevent negative deductions and deductions greater than 10 points.
+    DPointDeduct[DPointDeduct<0] <- 0
+    DPointDeduct[DPointDeduct>10] <- 10
+  }
+
+  # 2. Organic matter content (F)
+  if(is.na(surfaceOC)){
+    FPointDeduct <- 0
+  } else {
+    OMDF <- OMDTDF()
+    bounds <- OMDF[1,]
+    if(surfaceOC > bounds[1]){
+      FPointDeduct <- OMDF[1,2]
+    } else if(surfaceOC > bounds[2]){
+      FPointDeduct <- OMDF[2,2]
+    } else if(surfaceOC > bounds[3]){
+      FPointDeduct <- OMDF[3,2]
+    } else if(surfaceOC > bounds[4]){
+      FPointDeduct <- OMDF[4,2]
+    } else {
+      FPointDeduct <- OMDF[5,2]
+    }
+  }
+
+  # 3. Depth of topsoil (E)
+  if(is.na(depthOfTopSoil)){
+    EPointDeduct <- 0
+  } else {
+    DTSDF <- depthOfTopSoilDF()
+    bounds <- DTSDF[1,]
+    if(depthOfTopSoil > bounds[1]){
+      EPointDeduct <- DTSDF[1,2]
+    } else if(depthOfTopSoil > bounds[2]){
+      EPointDeduct <- DTSDF[2,2]
+    } else if(depthOfTopSoil > bounds[3]){
+      EPointDeduct <- DTSDF[3,2]
+    } else if(depthOfTopSoil > bounds[4]){
+      EPointDeduct <- DTSDF[4,2]
+    } else {
+      EPointDeduct <- DTSDF[5,2]
+    }
+  }
+
+  # 4. Surface reaction (pH) (V)
+  if(is.na(surfacepH)){
+    VPointDeduct <- 0
+  } else {
+    SRDF <- surfaceReactionDF()
+    bounds <- SRDF[1,]
+    if(surfacepH > bounds[1]){
+      VPointDeduct <- SRDF[1,2]
+    } else if(surfacepH > bounds[2]){
+      VPointDeduct <- SRDF[2,2]
+    } else if(surfacepH > bounds[3]){
+      VPointDeduct <- SRDF[3,2]
+    } else if(surfacepH > bounds[4]){
+      VPointDeduct <- SRDF[4,2]
+    } else if(surfacepH > bounds[5]){
+      VPointDeduct <- SRDF[5,2]
+    } else if(surfacepH > bounds[6]){
+      VPointDeduct <- SRDF[6,2]
+    } else if(surfacepH > bounds[7]){
+      VPointDeduct <- SRDF[7,2]
+    } else if(surfacepH > bounds[8]){
+      VPointDeduct <- SRDF[8,2]
+    } else {
+      VPointDeduct <- SRDF[9,2]
+    }
+  }
+
+  # 5. Surface salinity (dS/m) (N)
+  # This will change in the future to include user defined and crop specific
+  # parameters.
+  if(is.na(surfaceSalinity)){
+    NPointDeduct <- 0
+  } else {
+    SSDF <- surfaceSalinityDF()
+    bounds <- SSDF[1,]
+    if(surfaceSalinity < bounds[1]){
+      NPointDeduct <- SSDF[1,2]
+    } else if(surfaceSalinity < bounds[2]){
+      NPointDeduct <- SSDF[2,2]
+    } else if(surfaceSalinity < bounds[3]){
+      NPointDeduct <- SSDF[3,2]
+    } else {
+      NPointDeduct <- SSDF[4,2]
+    }
+  }
+
+  # 6. Surface sodicity (sodium adsorption ratio) (Y)
+  if(is.na(surfaceSodicity)){
+    YPointDeduct <- 0
+  } else {
+    SSDF <- surfaceSodicityDF()
+    bounds <- SSDF[1,]
+    if(surfaceSodicity < bounds[1]){
+      YPointDeduct <- SSDF[1,3]
+    } else if(surfaceSodicity < bounds[2]){
+      YPointDeduct <- SSDF[2,3]
+    } else if(surfaceSodicity < bounds[3]){
+      YPointDeduct <- SSDF[3,3]
+    } else if(surfaceSodicity < bounds[4]){
+      YPointDeduct <- SSDF[5,3]
+    } else {
+      YPointDeduct <- SSDF[6,3]
+    }
+  }
+
+  # 7. Depth of peat (cm) (O)
+  # if(is.na(depthOfPeat)){
+  #   OPointDeduct <- 0
+  # } else {
+  #   if(depthOfPeat >= 40){
+  #     OPointDeduct <- ((depthOfPeat - 10) * (sqrt(0.12))) / sqrt(bulkDensity)
+  #   } else {
+  #     OPointDeduct <- 0
+  #   }
+  # }
+
+  # 8. Return the deduction points for the interim soil rating (surface factors)
+  return(100 - sum(DPointDeduct,FPointDeduct,VPointDeduct))
+
+}
+
+#' Basic soil rating (subsurface factors)
+#'
+#' The basic soil rating determines a point deduction based on the subsurface
+#' parameters of the soil. There are five parameters for subsurface factors. These
+#' are impeding layer (structure and consistence) (D), contrasting texture,
+#' reaction (pH) (V), salinity (EC) (N), sodicity (SAR) (Y). Currently, E, N, Y,
+#' and O are not used. Future updates will include this additional information.
+#' @param subsurfaceSiltPercent Percentage of subsurface (depths 60-200cm) silt
+#' @param subsurfaceClayPercent Percentage of subsurface (depths 60-200cm) clay
+#' @param subsurfaceBulkDensity Subsurface bulk density
+#' @param impedingLayerDepth Subsurface impeding layers depth (cm)
+#' @param subsurfaceSalinity Subsurface salinity (depths 60-200cm)
+#' @param subsurfaceSodicity Subsurface sodicity (depths 60-200cm)
+#' @return Deduction points for basic soil rating.
+#' @export
+basicSoilRating <- function(subsurfaceSiltPercent,subsurfaceClayPercent,subsurfaceBulkDensity,impedingLayerDepth,subsurfacepH,subsurfaceSalinity,subsurfaceSodicity){
+
+  # 1. Structure and consistence (D)
+  # This will change in future updates
+  if(is.na(subsurfaceSiltPercent) || is.na(subsurfaceClayPercent) || is.na(subsurfaceBulkDensity)){
+    DPointDeduct <- 0
+  } else {
+    surfaceSandPercent <- 100 - surfaceSiltPercent - surfaceClayPercent
+    DPointDeduct <- surfaceOC
+    DPointDeduct[surfaceOC>3] <- 0
+    DPointDeduct[surfaceOC<=3] <- (3 / surfaceOC[surfaceOC<3]) + ((surfaceSandPercent[surfaceOC<3]) / 3 * surfaceOC[surfaceOC<3]) + surfaceSiltPercent[surfaceOC<3]
+
+    #Prevent negative deductions and deductions greater than 10 points.
+    DPointDeduct[DPointDeduct<0] <- 0
+    DPointDeduct[DPointDeduct>10] <- 10
+  }
+
+  # 7. Return the deduction points for the interim soil rating (surface factors)
+  return(100 - sum(DPointDeduct,FPointDeduct,VPointDeduct))
+
+}
+
