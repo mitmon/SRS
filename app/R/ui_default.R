72f1811c2c7edd34fce39ba0ae0fb0117926729b
mitmon <mitchkrafczek@gmail.com>
2022-04-11 10:04:57 -0400

    apr 11 2022

---
+++ app/R/ui_default.R
@@ -0,0 +1,18 @@
+Indices <- c("Climate" = "climate",
+             "Mineral soils" = "mineral",
+             "Organic soils" = "organic",
+             "Landscape" = "landscape")
+
+cropTypes <- c("Alfalfa" = "alfalfa",
+               "Canola" = "canola",
+               "Corn" = "corn",
+               "Potato" = "potato",
+               "Soybean" = "soybean",
+               "Spring Seeded-Small Grain" = "sssg")
+
+cropDefaultData <- c('alfalfaDefaultDF',
+                     'canolaDefaultDF',
+                     'cornDefaultDF',
+                     'potatoDefaultDF',
+                     'soybeanDefaultDF',
+                     'SSSGDefaultDF')
